6caf155a367dce58c14f981db78fe9db92f12b6d
Franco <jfranco8@outlook.com>
2022-06-21 13:16:22 -0500

    new apps

--- install.sh
+++ install.sh
@@ -202,12 +202,20 @@ while [ "$CHOICE -ne 4" ]; do
             sudo flatpak install -y flathub com.jetbrains.PyCharm-Professional
             # SQL client 
             sudo flatpak install -y flathub com.github.alecaddd.sequeler
+            #Teamviewer
+            wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm && sudo dnf -y install ./teamviewer.x86_64.rpm
+            # Firmware
+            sudo flatpak install -y flathub org.gnome.Firmware
+            # Decoder 
+            sudo flatpak install -y flathub com.belmoussaoui.Decoder
+            #ultimaker cura 
+            sudo flatpak install -y flathub com.ultimaker.cura
 
 
 
             # install software
             sudo dnf check-update
-            sudo dnf install -y gnome-extensions-app gnome-tweaks gnome-shell-extension-appindicator vlc dnfdragora mscore-fonts-all google-noto-sans-fonts neofetch cmatrix p7zip unzip gparted google-chrome-stable clang cmake variety microsoft-edge-stable code htop brave-browser notion-app-enhanced gnome-pomodoro teams lpf-spotify-client
+            sudo dnf install -y gnome-extensions-app gnome-tweaks gnome-shell-extension-appindicator vlc dnfdragora mscore-fonts-all google-noto-sans-fonts neofetch cmatrix p7zip unzip gparted google-chrome-stable clang cmake variety microsoft-edge-stable code htop brave-browser notion-app-enhanced gnome-pomodoro teams lpf-spotify-client prusa-slicer
             notify-send "Option 10 - Software has been installed" --expire-time=10
             ;;
         11)  echo "Installing Oh-My-Zsh"
